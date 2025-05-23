0c078ca02668f13723af6774733b470ade89e711
NicolasKDev <nicolaskelemen.contact@gmail.com>
2025-05-23 17:56:13 +0200

    Issue #1 Add order management for frontend.
    Also renaming "taskStore" in "tasksStore"

--- src/components/TaskCardList.vue
+++ src/components/TaskCardList.vue
@@ -1,24 +1,29 @@
 <template>
-    <draggable v-model="taskStore.tasks" item-key="id">
-        <template #item="{ element: storeTask, index }">
+    <draggable v-model="tasksStore.tasks" item-key="order" @end="onDragEnd">
+        <template #item="{ element: storeTask, index }" >
             <div class="flex items-center justify-between gap-2 max-w-1/2 mx-auto p-2 bg-white rounded shadow hover:bg-emerald-300">
-                <p>{{ id }}</p>
-                <task-card :task="storeTask" @update-task="taskStore.patchTask(storeTask)" @delete-task="taskStore.deleteTask(storeTask)"/>
+                <task-card :task="storeTask" @update-task="tasksStore.patchTask(storeTask)" @delete-task="tasksStore.deleteTask(storeTask)"/>
             </div>
         </template>
     </draggable>
 </template>
 
 <script setup>
-    import { useTaskStore } from '@/stores/task';
+    import { useTasksStore } from '@/stores/tasks';
     import { onMounted } from 'vue';
     import TaskCard from '@/components/TaskCard.vue';
     import draggable from 'vuedraggable';
     
-    const taskStore = useTaskStore();
+    const tasksStore = useTasksStore();
+
+    const onDragEnd = (event) => {
+        const newIndex = event.newIndex;
+        tasksStore.updateOrdersFromIndex();
+        tasksStore.patchTask(tasksStore.tasks[newIndex]);
+    }
     
     onMounted(async () => {
-        await taskStore.loadTasks();
+        await tasksStore.loadTasks();
     });
 
 </script>
